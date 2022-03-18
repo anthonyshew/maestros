a93271b76cafb62d140e6896218eea1c92924176
Sean C Davis <scdavis41@gmail.com>
2022-03-18 14:17:47 -0400

    Add more relevant content and clean up a few more styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,3 @@
-import Head from "next/head";
 import Link from "next/link";
 import { compareDesc, format, parseISO } from "date-fns";
 import { allPosts, Post } from "contentlayer/generated";
@@ -12,23 +11,27 @@ export async function getStaticProps() {
 
 function PostCard(post: Post) {
   return (
-    <div className="mb-6">
-      <time dateTime={post.date} className="block text-sm text-gray-600">
-        {format(parseISO(post.date), "LLLL d, yyyy")}
-      </time>
-      <h2 className="text-lg">
+    <div className="mb-8">
+      <h2 className="text-xl">
         <Link href={post.url}>
           <a className="text-blue-700 hover:text-blue-900">{post.title}</a>
         </Link>
       </h2>
+      <time dateTime={post.date} className="block text-xs text-gray-600 mb-2">
+        {format(parseISO(post.date), "LLLL d, yyyy")}
+      </time>
+      <div
+        className="text-sm"
+        dangerouslySetInnerHTML={{ __html: post.body.html }}
+      />
     </div>
   );
 }
 
 export default function Home({ posts }: { posts: Post[] }) {
   return (
-    <div className="max-w-2xl mx-auto py-8 text-center">
-      <h1 className="text-3xl font-bold mb-8">Next.js Example</h1>
+    <div className="max-w-xl mx-auto py-8">
+      <h1 className="text-3xl font-bold mb-8 text-center">Next.js Example</h1>
 
       {posts.map((post, idx) => (
         <PostCard key={idx} {...post} />
