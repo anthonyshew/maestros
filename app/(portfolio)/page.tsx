1c95540f37d17839a0a26de0d5034805c9caa2cf
Anthony Shew <anthony.shew@vercel.com>
2023-02-10 20:43:26 -0700

    Update packages.

--- app/(portfolio)/page.tsx
+++ app/(portfolio)/page.tsx
@@ -1,36 +1,13 @@
 import Link from "next/link";
-import { github } from "lib/github";
 
 export const revalidate = 3600; // hourly
 
 const getData = async () => {
-  const [pinned, kittr, personalRepos] = await Promise.all([
-    fetch("https://gh-pinned-repos.egoist.dev/?username=anthonyshew").then(
-      (res) => res.json()
-    ),
-    github.rest.repos.get({
-      owner: "kittrgg",
-      repo: "kittr",
-    }),
-    github.rest.repos.listForUser({
-      username: "anthonyshew",
-      sort: "pushed",
-    }),
-  ]);
+  const pins = await fetch(
+    "https://gh-pinned-repos.egoist.dev/?username=anthonyshew"
+  ).then((res) => res.json());
 
-  console.log(pinned);
-
-  return [
-    {
-      name: pinned[0].repo,
-      stargazers_count: pinned[0].stars,
-      forks: pinned[0].forks,
-      fork: false,
-      private: false,
-    },
-    kittr.data,
-    ...personalRepos.data,
-  ];
+  return pins;
 };
 
 export default async function Home() {
@@ -38,23 +15,7 @@ export default async function Home() {
 
   return (
     <div className="max-w-xl py-8 mx-auto">
-      <pre>
-        {JSON.stringify(
-          data
-            .filter((repo) => !repo.fork)
-            .filter((repo) => !repo.private)
-            .map((repo) => ({
-              name: repo.name,
-              forks: repo.forks,
-              stargazers: repo.stargazers_count,
-            })),
-          null,
-          2
-        )}
-      </pre>
-      {/* <pre>{data.data.stargazers_count}</pre>
-      <pre>{data.data.open_issues_count}</pre>
-      <pre>{data.data.description ?? "No description"}</pre> */}
+      <pre>{JSON.stringify(data, null, 2)}</pre>
       <h1 className="mb-8 text-3xl font-bold text-center">Next.js Example</h1>
       <div className="flex flex-col">
         <Link href="/blog">Blog Posts</Link>
--- lib/github.ts
+++
@@ -1,4 +0,0 @@
-import { Octokit } from 'octokit'
-import { env } from 'lib/env'
-
-export const github = new Octokit({ auth: env.GITHUB_PAT })
