ea9593b9998ed95c9b65981caabc3e6a6a56c26f
Anthony Shew <anthony.shew@vercel.com>
2023-06-29 02:34:45 -0700

    Intro monorepos.

--- app/(maestros)/monorepos/layout.tsx
+++ app/(maestros)/monorepos/layout.tsx
@@ -2,10 +2,8 @@ import { rubik } from "#/app/fonts";
 import type { Metadata } from "next";
 import AnalyticsWrapper from "#/components/Analytics";
 import { buildMeta } from "#/app/metadata";
-import { Navbar } from "#/components/Navbar";
 import "#/app/globals.css";
 import { ThemeWrapper } from "#/app/providers";
-import { ThemeController } from "#/components/ThemeController";
 
 export const generateMetadata = async (): Promise<Metadata> => {
   return await buildMeta({
@@ -26,10 +24,7 @@ export default function RootLayout({
     >
       <body>
         <ThemeWrapper>
-          <main className="relative flex flex-col flex-auto max-w-5xl min-h-screen px-4 pb-4 mx-auto sm:py-8 lg:py-20 md:flex-row">
-            <div className="absolute right-4 top-4 md:hidden">
-              <ThemeController />
-            </div>
+          <main className="relative flex flex-col justify-center flex-auto min-h-screen px-4 pb-4 mx-auto sm:py-8 lg:py-20 md:flex-row">
             {children}
           </main>
         </ThemeWrapper>
