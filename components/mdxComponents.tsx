62efc49553bd9e18e5066acb55511d229e2d95e2
Anthony Shew <anthony.shew@vercel.com>
2023-07-11 06:55:02 -0700

    Set up shiki.

--- components/mdxComponents.tsx
+++ components/mdxComponents.tsx
@@ -3,6 +3,8 @@ import Balancer from "react-wrap-balancer";
 import { LinkHeading } from "#/components/LinkHeading";
 import type { ImageProps } from "next/image";
 import { TwoColumns, TwoColumnsProps } from "#/components/TwoColumns";
+import { getHighlighter } from "shiki";
+import { ReactNode } from "react";
 
 interface CustomImageProps extends ImageProps {
   containerClassName: string;
@@ -46,5 +48,26 @@ export const mdxComponents = {
   },
   h6: ({ children }: { children: string }) => {
     return <LinkHeading component="h6">{children}</LinkHeading>;
+  },
+  // Not in love with this but it works. ✨
+  pre: async (props: {
+    // I'm sorry, excuse me, what?...I don't care right now.
+    children: { props: { children: string; className: string } };
+  }) => {
+    const code = props.children.props.children;
+    const lang = props.children.props.className.replace("language-", "");
+
+    return (
+      <div
+        dangerouslySetInnerHTML={{
+          __html: await getHighlighter({
+            theme: "github-dark-dimmed",
+            langs: ["json", "ts", "tsx"],
+          }).then((highlighter) => {
+            return highlighter.codeToHtml(code, { lang });
+          }),
+        }}
+      />
+    );
   },
 };
