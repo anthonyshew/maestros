f64324079c75dab85687e262bc401b1747c9e463
Anthony Shew <anthony.shew@vercel.com>
2023-07-16 22:33:19 -0700

    Kinda close.

---
+++ app/(maestros)/monorepos/email-list/page.tsx
@@ -0,0 +1,24 @@
+import { prisma } from "#/prisma";
+import { getServerSession } from "next-auth";
+import { authOptions } from "#/app/api/auth/[...nextauth]/route";
+
+const Page = async () => {
+  const session = await getServerSession(authOptions);
+
+  const isAnthony = session?.user?.email === "anthonyshew@gmail.com";
+
+  if (!isAnthony) {
+    throw new Error("You're not Anthony!");
+  }
+
+  const peeps = await prisma.user.findMany();
+  const emails = peeps.map((peep) => peep.email);
+
+  return (
+    <div>
+      <pre>{JSON.stringify(emails, null, 2)}</pre>
+    </div>
+  );
+};
+
+export default Page;
---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,21 @@
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import NextAuth from 'next-auth'
+import GitHub from 'next-auth/providers/github'
+import prisma from '#/prisma'
+import type { NextAuthOptions } from 'next-auth'
+
+if (!process.env.GH_CLIENT_ID || !process.env.GH_CLIENT_SECRET) {
+  throw new Error("You're missing GitHub variables.")
+}
+
+export const authOptions: NextAuthOptions = {
+  // @ts-expect-error
+  adapter: PrismaAdapter(prisma),
+  providers: [GitHub({
+    clientId: process.env.GH_CLIENT_ID,
+    clientSecret: process.env.GH_CLIENT_SECRET
+  })],
+}
+
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST }
