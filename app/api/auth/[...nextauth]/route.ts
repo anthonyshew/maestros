145fbada577efd66235cd789fead9002165f3f0e
Anthony Shew <anthony.shew@vercel.com>
2023-07-17 12:36:53 -0700

    Fake with credentials provider?

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,7 @@
 import { PrismaAdapter } from '@auth/prisma-adapter';
 import NextAuth from 'next-auth'
 import GitHub from 'next-auth/providers/github'
+import CredentialsProvider from 'next-auth/providers/credentials';
 import prisma from '#/prisma'
 import type { NextAuthOptions } from 'next-auth'
 
@@ -11,11 +12,32 @@ if (!process.env.GH_CLIENT_ID || !process.env.GH_CLIENT_SECRET) {
 export const authOptions: NextAuthOptions = {
   // @ts-expect-error
   adapter: PrismaAdapter(prisma),
-  providers: [GitHub({
-    clientId: process.env.GH_CLIENT_ID,
-    clientSecret: process.env.GH_CLIENT_SECRET
-
-  })],
+  providers: [
+    process.env.VERCEL_ENV === "preview"
+      ? CredentialsProvider({
+        name: "Credentials",
+        credentials: {
+          username: {
+            label: "Username",
+            type: "text",
+            placeholder: "ashew",
+          },
+          password: { label: "Password", type: "password" },
+        },
+        async authorize() {
+          return {
+            id: "1",
+            name: "Anthony Shew",
+            email: "anthony@shew.dev",
+            image: "https://i.pravatar.cc/150?u=jsmith@example.com",
+          }
+        },
+      }) :
+      GitHub({
+        clientId: process.env.GH_CLIENT_ID,
+        clientSecret: process.env.GH_CLIENT_SECRET
+      }),
+  ],
 }
 
 const handler = NextAuth(authOptions);
