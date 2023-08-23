d635fbeb17f6673f0b1a15886f752fc4f5867df2
Anthony Shew <anthony.shew@vercel.com>
2023-08-22 22:02:53 -0700

    Distributing flags out to workspaces.

--- tooling/typescript-config/.eslintrc.cjs
+++ tooling/typescript-config/.eslintrc.cjs
@@ -1,7 +1,7 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
-  extends: [require.resolve('@repo/lint/node')],
+  extends: [require.resolve('../eslint-config/node.cjs')],
   ignorePatterns: ['./src/components'],
   parserOptions: {
     project: `${__dirname}/tsconfig.json`,
