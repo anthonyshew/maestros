06fba4523847fca480f8ad6844b64f46492a5a16
Anthony Shew <anthony.shew@vercel.com>
2023-08-24 19:15:04 -0700

    Some ignoring.

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -5,5 +5,5 @@ const globalForPrisma = global as unknown as { prisma?: PrismaClient };
 export const prisma = globalForPrisma.prisma || new PrismaClient();
 
 // Covering for a turbo bug!
-// eslint-disable-next-line turbo/no-undeclared-env-vars
+ 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
