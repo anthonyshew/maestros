56358f7696328c0302fce15b877e1762911a2d53
Anthony Shew <anthony.shew@vercel.com>
2023-08-25 11:57:27 -0700

    A couple formats.

--- packages/ui/tsup.config.ts
+++ packages/ui/tsup.config.ts
@@ -1,15 +1,15 @@
-import { execSync } from 'node:child_process'
-import { Options, defineConfig } from 'tsup'
+import { execSync } from 'node:child_process';
+import { Options, defineConfig } from 'tsup';
 
 export default defineConfig((options: Options) => ({
-  entry: ['src/index.ts', "src/server.ts"],
+  entry: ['src/index.ts', 'src/server.ts'],
   clean: true,
   onSuccess: async () => {
-    execSync("tsc --emitDeclarationOnly")
+    execSync('tsc --emitDeclarationOnly');
   },
   // These are false for preserving directives
   // https://github.com/egoist/tsup/issues/835#issuecomment-1594905290
   splitting: false,
   treeshake: false,
-  ...options
-}))
+  ...options,
+}));
