532461644e906aeffc0b61d5b7d0ab5bd528914c
Anthony Shew <anthony.shew@vercel.com>
2023-09-09 12:38:24 -0700

    I'm happy with that.

--- apps/maestros/components/Flow/nodes.tsx
+++ apps/maestros/components/Flow/nodes.tsx
@@ -5,12 +5,12 @@ import { Handle, Position } from 'reactflow';
 
 function _GroupNode({ data }: { data: Record<string, any> }) {
   return (
-    <div className="relative z-10 w-full h-full bg-gradient-to-br from-yellow-600/80 to-yellow-400/0 dark:from-yellow-700 dark:to-yellow-400/0 to rounded-3xl">
-      <div className="absolute z-10 bg-gradient-155 from-50% to-yellow-300/20 dark:to-[rgb(83,76,51)]/90 from-white dark:from-black inset-2 rounded-2xl">
-        <div className="mt-6 ml-6 text-4xl font-semibold text-left text-black dark:text-white">
-          {data.label}
-        </div>
+    <div className="relative z-10 w-full h-full bg-gradient-155 border-2 border-yellow-200/80 dark:border-yellow-300/20 from-50% to-yellow-300/20 dark:to-[rgb(83,76,51)]/90 from-transparent rounded-3xl">
+      {/* <div className="absolute z-10 bg-gradient-155 from-50% to-yellow-300/20 dark:to-[rgb(83,76,51)]/90 from-white dark:from-black inset-2 rounded-2xl"> */}
+      <div className="mt-6 ml-6 text-4xl font-semibold text-left text-black dark:text-white">
+        {data.label}
       </div>
+      {/* </div> */}
     </div>
   );
 }
@@ -27,7 +27,7 @@ function _LinkedNode({ data }: { data: Record<string, any> }) {
 }
 function _UnlinkedNode({ data }: { data: Record<string, any> }) {
   return (
-    <div className="z-30 px-4 py-2 bg-white border-2 border-yellow-500 rounded-md shadow-md dark:bg-black">
+    <div className="px-4 py-2 bg-white border-2 border-yellow-500 rounded-md shadow-md dark:bg-black">
       <div className="flex items-center justify-center font-medium text-black rounded-full dark:text-white">
         {data.label}
       </div>
