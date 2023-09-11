d419f8377f0fc9dc2bb0e095ece5c429508ad3ad
Anthony Shew <anthony.shew@vercel.com>
2023-09-10 22:42:43 -0700

    Getting data just fine.

--- apps/viz/app/page.tsx
+++ apps/viz/app/page.tsx
@@ -2,16 +2,16 @@ import '../components/styles.css';
 import { execSync } from 'node:child_process';
 
 export default async function Page() {
-  const graphBuffer = await execSync(`turbo build --dry=json`);
-  const graph = await graphBuffer.toString();
+  const graphBuffer = await execSync(`turbo build --dry=json`).toString();
+  const graph = JSON.parse(graphBuffer.toString());
+
+  const renderMe = graph.tasks[0];
 
   return (
-    <html lang="en">
-      <body className="flex flex-col items-center justify-center min-h-screen text-white bg-black">
-        <div>
-          <pre>{JSON.stringify(graph, null, 2)}</pre>
-        </div>
-      </body>
-    </html>
+    <div>
+      <pre className="w-screen h-screen overflow-x-hidden overflow-y-auto text-white">
+        {JSON.stringify(renderMe, null, 2)}
+      </pre>
+    </div>
   );
 }
