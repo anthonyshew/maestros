3474217d1de3e4b3dd27a45014ee76bff08863fe
Anthony Shew <anthony.shew@vercel.com>
2023-09-15 01:16:33 -0700

    Remove console.log.

--- apps/studio/middleware.ts
+++ apps/studio/middleware.ts
@@ -2,7 +2,6 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
-  console.log("middy")
   return NextResponse.redirect(new URL('/build', request.url))
 }
 
