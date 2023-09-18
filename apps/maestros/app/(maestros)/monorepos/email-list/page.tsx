7ede9d325b4dc5150397b162e6514f64b48f7b8d
Anthony Shew <anthony.shew@vercel.com>
2023-09-17 23:24:50 -0700

    Add back email list check.

--- apps/maestros/app/(maestros)/monorepos/email-list/page.tsx
+++ apps/maestros/app/(maestros)/monorepos/email-list/page.tsx
@@ -5,13 +5,12 @@ import { authOptions } from '#/app/api/auth/[...nextauth]/route';
 export const revalidate = 0;
 
 const Page = async () => {
-  // const session = await getServerSession(authOptions);
+  const session = await getServerSession(authOptions);
+  const isAnthony = session?.user?.email === 'anthonyshew@gmail.com';
 
-  // const isAnthony = session?.user?.email === 'anthonyshew@gmail.com';
-
-  // if (!isAnthony) {
-  //   throw new Error("You're not Anthony!");
-  // }
+  if (!isAnthony) {
+    throw new Error("You're not Anthony!");
+  }
 
   const peeps = await prisma.user.findMany();
   const emails = peeps.map((peep) => peep.email);
