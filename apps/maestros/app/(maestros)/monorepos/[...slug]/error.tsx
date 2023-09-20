58947532673d1b2f4d1c3016a40885d441a3dce3
Anthony Shew <anthony.shew@vercel.com>
2023-09-20 01:16:08 -0700

    Vercel CI/CD.

--- apps/maestros/app/(maestros)/monorepos/[...slug]/error.tsx
+++ apps/maestros/app/(maestros)/monorepos/[...slug]/error.tsx
@@ -2,7 +2,7 @@
 
 export default function Error() {
   return (
-    <div className="prose dark:prose-invert">
+    <div className="mt-24 ml-8 prose dark:prose-invert">
       <h1>Radio silence...</h1>
       <h2>404</h2>
       <p>It doesn't look like there is anything here.</p>
