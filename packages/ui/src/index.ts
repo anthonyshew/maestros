ebd676cc6c5bcfd62da8ef631ba39daa3b604f3e
Anthony Shew <anthony.shew@vercel.com>
2023-09-21 19:21:14 -0700

    Okie.

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,15 +1,14 @@
-'use client';
+'use client'
 
 export * from './components/Avatar';
+export * from './components/Callout';
 export * from './components/Navbar';
 export * from './components/Sheet';
 export * from './components/Skeleton';
-export * from './components/Callout';
 export * from './components/TwoColumns';
 export * from './components/Button';
 export * from './components/LinkHeading';
 export * from './components/Icons';
 export * from './components/RepoCard';
 export * from './components/IconBox';
-export * from './components/FileTree';
 export * from './components/Discord';
