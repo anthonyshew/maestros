69fe1fc8c850ae622bd0a0358b6d3e0967e29b0e
Anthony Shew <anthony.shew@vercel.com>
2023-09-23 15:41:12 -0700

    Make ESLint happy.

--- apps/starter-vite/.eslintrc.cjs
+++ apps/starter-vite/.eslintrc.cjs
@@ -1,19 +1,9 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+  extends: [require.resolve('@repo/lint/vite.js')],
+  parserOptions: {
+    project: [`${__dirname}/tsconfig.json`, `${__dirname}/tsconfig.node.json`],
   },
+  plugins: ['@typescript-eslint'],
 };
