38a06fe7ffb5c34de34c33ba97cac270c6a06dcb
Anthony Shew <anthony.shew@vercel.com>
2023-09-22 18:24:59 -0700

    Clean up tsconfigs.

--- starters/nextjs-starter/tailwind.config.ts
+++ starters/nextjs-starter/tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
@@ -57,6 +57,6 @@ const config: Config = {
     },
   },
   plugins: [],
-}
+};
 
-export default config
+export default config;
