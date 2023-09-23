911cfd68fa80558b651b67c74bf2ab2ffe46acec
Anthony Shew <anthony.shew@vercel.com>
2023-09-22 19:09:57 -0700

    Set up Next.js starter.

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@
 module.exports = {
   root: true,
   extends: [require.resolve('./tooling/eslint-config/node.js')],
-  ignorePatterns: ['apps/**', 'packages/**', 'tooling/**', 'starters/**'],
+  ignorePatterns: ['apps/**', 'packages/**', 'tooling/**'],
   parserOptions: {
     tsconfigRootDir: __dirname,
     project: true,
