0e2df69b722a0de283258d92deee7b4a263b6c0b
Anthony Shew <anthony.shew@vercel.com>
2023-09-25 22:51:09 -0700

    Wow, that's nuts.

--- apps/maestros/app/(maestros)/monorepos/api/og/route.tsx
+++ apps/maestros/app/(maestros)/monorepos/api/og/route.tsx
@@ -19,71 +19,69 @@ export async function GET(req: Request) {
   const marginY = marginX + 40;
 
   return new ImageResponse(
-    (
+    <div
+      style={{
+        height: '100%',
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'flex-start',
+        justifyContent: 'flex-start',
+        backgroundImage: 'url("https://shew.dev/images/maestros/og-bg.png")',
+        backgroundSize: '100% 100%',
+        backgroundPosition: 'center',
+      }}
+    >
       <div
         style={{
-          height: '100%',
-          width: '100%',
+          marginLeft: marginX,
+          marginRight: marginX,
+          marginTop: marginY,
+          display: 'flex',
+          fontSize: 100,
+          fontFamily: 'Inter',
+          color: 'white',
+          whiteSpace: 'pre-wrap',
+        }}
+        tw="text-8xl"
+      >
+        {title}
+      </div>
+      <div
+        style={{
           display: 'flex',
           flexDirection: 'column',
-          alignItems: 'flex-start',
-          justifyContent: 'flex-start',
-          backgroundImage: 'url("https://shew.dev/images/maestros/og-bg.png")',
-          backgroundSize: '100% 100%',
-          backgroundPosition: 'center',
+          justifyContent: 'center',
+          alignItems: 'center',
+          marginLeft: marginX,
         }}
       >
-        <div
-          style={{
-            marginLeft: marginX,
-            marginRight: marginX,
-            marginTop: marginY,
-            display: 'flex',
-            fontSize: 100,
-            fontFamily: 'Inter',
-            color: 'white',
-            whiteSpace: 'pre-wrap',
-          }}
-          tw="text-8xl"
-        >
-          {title}
-        </div>
-        <div
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            alignItems: 'center',
-            marginLeft: marginX,
-          }}
-        >
-          {subtitle ? (
-            <div
-              style={{ fontFamily: 'Inter' }}
-              tw="text-6xl text-slate-300 mt-8"
-            >
-              {subtitle}
-            </div>
-          ) : null}
-        </div>
-        <div
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            alignItems: 'flex-start',
-            marginLeft: marginX,
-          }}
-        >
-          {title !== 'Monorepo Maestros' ? (
-            <div tw="text-yellow-400 text-6xl mt-60">Monorepo Maestros</div>
-          ) : null}
-          <div tw="text-yellow-400 text-5xl mt-8">
-            The ultimate monorepo reference.
+        {subtitle ? (
+          <div
+            style={{ fontFamily: 'Inter' }}
+            tw="text-6xl text-slate-300 mt-8"
+          >
+            {subtitle}
           </div>
+        ) : null}
+      </div>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'flex-start',
+          marginLeft: marginX,
+        }}
+      >
+        {title !== 'Monorepo Maestros' ? (
+          <div tw="text-yellow-400 text-6xl mt-60">Monorepo Maestros</div>
+        ) : null}
+        <div tw="text-yellow-400 text-5xl mt-8">
+          The ultimate monorepo reference.
         </div>
       </div>
-    ),
+    </div>,
     {
       width: 1920,
       height: 1080,
--- apps/maestros/app/api/og/route.tsx
+++ apps/maestros/app/api/og/route.tsx
@@ -19,71 +19,69 @@ export async function GET(req: Request) {
   const titleIsMyName = title === 'Anthony Shew';
 
   return new ImageResponse(
-    (
+    <div
+      style={{
+        height: '100%',
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundImage: 'url("https://shew.dev/images/og-bg.png")',
+        backgroundSize: '100% 100%',
+        backgroundPosition: 'center',
+      }}
+    >
       <div
         style={{
-          height: '100%',
-          width: '100%',
+          marginLeft: 190,
+          marginRight: 190,
+          marginBottom: 70,
           display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          justifyContent: 'center',
-          backgroundImage: 'url("https://shew.dev/images/og-bg.png")',
-          backgroundSize: '100% 100%',
-          backgroundPosition: 'center',
+          fontSize: 140,
+          color: 'white',
+          fontFamily: 'Inter Bold',
+          whiteSpace: 'pre-wrap',
         }}
       >
+        {title}
+      </div>
+      {subtitle ? (
         <div
           style={{
             marginLeft: 190,
             marginRight: 190,
-            marginBottom: 70,
+            marginBottom: 120,
             display: 'flex',
-            fontSize: 140,
-            color: 'white',
-            fontFamily: 'Inter Bold',
+            fontSize: 50,
+            fontFamily: 'Inter',
             whiteSpace: 'pre-wrap',
           }}
+          tw="text-slate-300"
         >
-          {title}
+          {subtitle}
         </div>
-        {subtitle ? (
-          <div
-            style={{
-              marginLeft: 190,
-              marginRight: 190,
-              marginBottom: 120,
-              display: 'flex',
-              fontSize: 50,
-              fontFamily: 'Inter',
-              whiteSpace: 'pre-wrap',
-            }}
-            tw="text-slate-300"
-          >
-            {subtitle}
-          </div>
-        ) : null}
-        <div
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}
-        >
-          <img
-            height={250}
-            src="https://shew.dev/images/me.jpg"
-            tw="rounded-full mb-10"
-            width={250}
-          />
-          <div style={{ fontFamily: 'Inter' }} tw="text-7xl text-white">
-            {titleIsMyName ? '' : 'Anthony Shew'}
-          </div>
-          <div tw="text-6xl text-slate-400 mt-8">{tagline}</div>
+      ) : null}
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <img
+          height={250}
+          src="https://shew.dev/images/me.jpg"
+          tw="rounded-full mb-10"
+          width={250}
+        />
+        <div style={{ fontFamily: 'Inter' }} tw="text-7xl text-white">
+          {titleIsMyName ? '' : 'Anthony Shew'}
         </div>
+        <div tw="text-6xl text-slate-400 mt-8">{tagline}</div>
       </div>
-    ),
+    </div>,
     {
       width: 1920,
       height: 1080,
