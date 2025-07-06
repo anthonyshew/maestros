1a46f75ac5607a9fde19cc2b40b2ff0ba9a43fd1
Anthony Shew <anthony.shew@vercel.com>
2025-07-06 13:02:55 -0600

    Delete "studio" code (#112)
    I'm building this right into Turborepo in
    https://github.com/vercel/turborepo/pull/10638

--- apps/studio/app/[...task]/ClientErrorProxy.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-export function ClientErrorProxy({ error }: { error: string }) {
-	throw new Error(error);
-	// biome-ignore lint/correctness/noUnreachable: Ignored when migrating
-	return null;
-}
